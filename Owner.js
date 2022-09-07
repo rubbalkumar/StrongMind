e23178602e633cc663be181c7b96d79ff4c352cb
rubbalkumar <35452158+rubbalkumar@users.noreply.github.com>
2022-09-07 09:51:50 -0700

    Update Owner.js

--- Owner.js
+++ Owner.js
@@ -8,7 +8,7 @@
 	};
 
     function allToppings(){
-        var url = "https:/dark-teal-cygnet-tux.cyclic.app//toppings";
+        var url = "https:/dark-teal-cygnet-tux.cyclic.app/toppings";
         fetch(url)
                 .then(checkStatus)
                 .then(function(responseText) {   
@@ -108,7 +108,7 @@
             headers: {'Content-Type': 'application/json'},
             body : data
         };
-        var url = "https:/dark-teal-cygnet-tux.cyclic.app//deletetopping";
+        var url = "https:/dark-teal-cygnet-tux.cyclic.app/deletetopping";
 		fetch(url, fetchOptions)
 			.then(checkStatus)
 			.then(function(responseText) {
@@ -152,7 +152,7 @@
                 headers: {'Content-Type': 'application/json'},
                 body : data
             };
-            var url = "https:/dark-teal-cygnet-tux.cyclic.app//updatetopping";
+            var url = "https:/dark-teal-cygnet-tux.cyclic.app/updatetopping";
 		fetch(url, fetchOptions)
 			.then(checkStatus)
 			.then(function(responseText) {
@@ -195,7 +195,7 @@
                 headers: {'Content-Type': 'application/json'},
                 body : data
             };
-            var url = "https:/dark-teal-cygnet-tux.cyclic.app//topping";
+            var url = "https:/dark-teal-cygnet-tux.cyclic.app/topping";
 		fetch(url, fetchOptions)
 			.then(checkStatus)
 			.then(function(responseText) {
